a8dac2325c92f68ee391b16a0bfc45944dada4e5
khafiluhur <khafiluhur.99@gmail.com>
2022-04-10 12:57:03 +0700

    fix

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://bengkel.shobaro.com/';
+    public $baseURL = 'bengkel.shobaro.com';
 
     /**
      * --------------------------------------------------------------------------
